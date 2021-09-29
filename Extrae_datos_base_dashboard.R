8159b9dfac34625aab0576260723675af025a77a
sectyop <71723199+sectyop@users.noreply.github.com>
2021-09-29 11:15:01 -0300

    Add files via upload

--- Extrae_datos_base_dashboard.R
+++ Extrae_datos_base_dashboard.R
@@ -135,7 +135,7 @@ ftpUpload(what = local_file, to = destino)
 
 # Escribo status ejecución en hoja "Status Datos SSGM"
 # --------------------------------------------------------------------------------
-gs4_auth(email = "nacho.ls@gmail.com")
+gs4_auth(email = "datos.ssgm@gmail.com")
 id_status = "1BwZjmRPRaFahxI8eWwU6EXAqsSUj7OU2_xWe6KT9u2Y"
 df_hora_guardado = as.data.frame(Sys.time() - hours(3))
 colnames(df_hora_guardado) = "FechaHoraActual"
